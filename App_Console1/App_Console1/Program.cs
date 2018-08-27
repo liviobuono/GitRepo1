793424e0c4107acd57172df3b969f134411a635e
livio.buono74 <livio.buono74@gmail.com>
2018-08-27 18:09:40 +0200

    Modifica

---
+++ App_Console1/App_Console1/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("modifica");
+            Console.ReadKey();
+
+            
+    }
+}
